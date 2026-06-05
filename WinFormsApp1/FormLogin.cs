c016ce99cee14be667b3c8bab09bd32322c140db
User@WIN-1STBJGFL3G0 <dmitrig232@gmail.com>
2026-06-05 13:56:27 +0500

    Add project files.

---
+++ WinFormsApp1/FormLogin.cs
@@ -0,0 +1,69 @@
+﻿using WinFormsApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShoesProject
+{
+    public partial class FormLogin : Form
+    {
+        public User CurrentUser { get; private set; }
+        public bool IsGuest { get; private set; }
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtLogin.Text) || string.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                MessageBox.Show("Введите логин и пароль", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (var db = new DbShopContext())
+            {
+                var user = db.Users
+                    .Where(w => w.Login == txtLogin.Text && w.Pass == txtPassword.Text)
+                    .FirstOrDefault();
+
+                if (user != null)
+                {
+                    CurrentUser = user;
+                    IsGuest = false;
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Неверный логин или пароль", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void BtnGuest_Click(object sender, EventArgs e)
+        {
+            CurrentUser = null;
+            IsGuest = true;
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
